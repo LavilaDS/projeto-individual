1af878b3963bc39bd5769aabdc4db4bf3a3de7b8
Leandro Lávila Dias Souza <leandrolavila16@gmail.com>
2025-03-07 01:17:59 +0000

    feat: adicionar método para retornar o usuario baseado no token

--- api/controllers/controladorUsuario.js
+++ api/controllers/controladorUsuario.js
@@ -1,7 +1,25 @@
 const { bancoDadosLogin } = require('../database/bancoDados.js');
 
-const obterUsuarioAtual = (req, res) => {
-    res.status(200).json({sucess:true});
+const  { validarToken } = require('../utils/validarToken.js')
+
+const obterUsuarioAtual = async (req, res) => {
+    try{
+        const token = req.cookies.tokenAcesso;
+        console.log(token)
+        console.log("ASDSAD")
+        const resultado = await validarToken(token);
+
+        if(resultado.sucesso){
+            console.log(resultado.dados)
+        } else {
+            return res.status(401).json({sucesso:false, mensagem:resultado.mensagem})
+        }
+        res.status(200).json({sucess:true, dados:resultado.dados});
+    }catch(err){
+        // console.log(err)
+        res.status(500).json({sucesso:false, messagem:"Erro desconhecido"})
+    }
+
 };
 
 
